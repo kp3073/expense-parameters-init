32f11922e7e04804d4684c20ecdf22bdb501e6e1
kp3073 <keyurpatel8288@gmail.com>
2024-11-03 23:48:58 -0500

    new111

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 for par in DB_HOST,prod.rds.endpoint ; do
   var=$(aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text)
-  echo export $(echo $par | awk -F , '{print $1}')=$var
+  echo export $(echo $par | awk -F , '{print $1}')=$var >>/config/param
 done
 #
 #--query 'parameter.Value' --output text
