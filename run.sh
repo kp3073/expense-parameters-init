9dfa15cdbfff77d1d293cff1cc9591475295249f
kp3073 <keyurpatel8288@gmail.com>
2024-11-03 23:44:17 -0500

    new111

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 for par in DB_HOST,prod.rds.endpoint ; do
-  aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text
+  var=$(aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text)
+  echo export $(echo $par | awk -F , '{print $1}')=$var
 done
-
+#
 #--query 'parameter.Value' --output text
