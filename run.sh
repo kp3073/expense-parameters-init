08ffa9d1e6fe45082b51dfeb08b0cfb66b9db59d
kp3073 <keyurpatel8288@gmail.com>
2024-11-07 11:09:29 -0500

    1

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 for par in ${INPUT} ; do
-  var=$(aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text)
+  var=$(aws ssm get-parameter --region us-east-1 --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text)
   echo export \"$(echo $par | awk -F , '{print $1}')=$var\" >>/config/params
 done
 #
